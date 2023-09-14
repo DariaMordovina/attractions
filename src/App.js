f27a15df4a11def0385a982382b56131ad24f687
“DariaMordovina” <Chikalova.d.a@yandex.ru>
2023-09-14 17:37:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[hotels,setHotels]=useState(data);
+  console.log(data)
+  const[showText,setShowText]=useState(false)
+  const[showMore,setShowMore]=useState(false)
+
+  const removeHotels =(id)=>{
+    let newHotels=hotels.filter(item=>item.id!==id);
+    setHotels(newHotels)
+  }
+  const showTextClick=(element)=>{
+   element.showMore= !element.showMore
+   setShowText(!showText)
+  }
+  return (<div>
+
+    <div className="container">
+     <h1>Nyc top {hotels.length} hotels</h1>
     </div>
+
+{hotels.map((element =>{
+  const {id,hotelName, description,image,source, showMore}=element;
+  return(
+    <div key={id}>
+<div className="container">
+    <h2>{id} - {hotelName}</h2>
+</div>
+
+<div className="container">
+    <p>{showMore ? description:description.substring(0,242)+"....."}
+    <button onClick={()=>showTextClick(element)}>{showMore?"show less":"show more"}</button>
+    </p>
+</div>
+
+<div className="container">
+    <img src="{image}" width="400px"  />
+</div>
+
+<div className="container">
+    <p>{source}</p>
+</div>
+
+<div className="container">
+   <button className='btn' onClick={()=>removeHotels(id)}>Remove</button>
+</div>
+
+    </div>
+  )
+}))}
+</div>
   );
 }
 
